2f1a2cadecbfd76a891d9d148c67bab0190c8b88
poliya <absentmedo@gmail.com>
2017-12-26 07:00:38 -0800

    Update HelperClass.java

--- voyage/src/ModulController/HelperClass.java
+++ voyage/src/ModulController/HelperClass.java
@@ -1,12 +1,41 @@
 package ModulController;
 
+import java.awt.Color;
 import java.sql.*;
 
 public abstract class HelperClass {
 	public static Connection c;
-
+	public static Color menuColorDefault=new Color(05, 05, 30);
 	private HelperClass() {}
 	
+	public static boolean executeAjouterSupprimerModifier(Connection c,String query) {
+		Statement st;
+		try {
+			st = c.createStatement();
+			
+		  int i = st.executeUpdate(query);
+		  System.out.println(i);
+		       if (i>0){
+		    	   return true;
+		       }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static ResultSet executeSelection(Connection c,String query) {
+		Statement st;
+		try {
+			st = c.createStatement();
+			return st.executeQuery(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 	public static void createDBConnection() {
 		try {
@@ -33,5 +62,27 @@ public abstract class HelperClass {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	public static int toTinyInt(boolean b) {
+		if(b)
+			return 1;
+		else
+			return 0;
+	}
+	public static int toTinyInt(String b) {
+		if(b.equals("true") || b.equals("1"))
+			return 1;
+		else if(b.equals("false") || b.equals("0"))
+			return 0;
+		else 
+			return 99999;
+	}
+	public static boolean toBoolean(int i) {
+		if(i==0)
+			return true;
+		else if(i==1)
+			return false;
+		else 
+			return false;
 	}
 }
